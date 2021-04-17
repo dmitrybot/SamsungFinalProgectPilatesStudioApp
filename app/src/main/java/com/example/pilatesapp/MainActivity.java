f6c54d192512d4e7e5aa13493d24d7eca243984b
Just Man <N.Mikhail2002@gmail.com>
2021-04-18 00:29:17 +0300

    change fragments function

--- app/src/main/java/com/example/pilatesapp/MainActivity.java
+++ app/src/main/java/com/example/pilatesapp/MainActivity.java
@@ -5,17 +5,20 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
 
     @Override
@@ -26,12 +29,48 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         drawer = findViewById(R.id.drawer_layout);
-
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
     }
+
+    /*@Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_notivications:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new      ).commit();
+                break;
+            case R.id.nav_timetable:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new      ).commit();
+                break;
+            case R.id.nav_news:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new      ).commit();
+                break;
+            case R.id.nav_reports:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new      ).commit();
+                break;
+            case R.id.nav_map:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new      ).commit();
+                break;
+            case R.id.nav_contacts:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new      ).commit();
+                break;
+            case R.id.nav_homepage:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new      ).commit();
+                break;
+            case R.id.nav_info:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new      ).commit();
+                break;
+            case R.id.nav_appInfo:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new      ).commit();
+                break;
+        }
+        return true;
+    }*/
+
     @Override
     public void onBackPressed(){
         if (drawer.isDrawerOpen(GravityCompat.START)){
