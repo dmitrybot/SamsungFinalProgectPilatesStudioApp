cfb4fc0bd60a96be4cc28e2c20d9c4c0f455d14c
Just Man <N.Mikhail2002@gmail.com>
2021-04-17 22:35:49 +0300

    extra toolbar

--- app/src/main/java/com/example/pilatesapp/MainActivity.java
+++ app/src/main/java/com/example/pilatesapp/MainActivity.java
@@ -42,11 +42,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    /*@Override
+    @Override
     public boolean onCreateOptionsMenu(Menu menu){
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_main,menu);
+        inflater.inflate(R.menu.extra_menu,menu);
         return true;
-    }*/
+    }
 
 }
