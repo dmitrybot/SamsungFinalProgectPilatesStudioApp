a543a09507927a034406bee23ba72d122baacf07
Just Man <N.Mikhail2002@gmail.com>
2021-04-20 02:46:05 +0300

    maps added

--- app/src/main/java/shinepilates/app/pilatesapp/fragments/MapsFragment.java
+++ app/src/main/java/shinepilates/app/pilatesapp/fragments/MapsFragment.java
@@ -9,13 +9,45 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import shinepilates.app.pilatesapp.R;
 
-public class MapsFragment extends Fragment {
+public class MapsFragment extends Fragment{
+
+    GoogleMap map;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_maps, container, false);
         final TextView textView = root.findViewById(R.id.text);
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.maps);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+                    @Override
+                    public void onMapClick(LatLng latLng) {
+                        MarkerOptions markerOptions = new MarkerOptions();
+                        markerOptions.position(latLng);
+                        markerOptions.title(latLng.latitude + " : " + latLng.longitude);
+                        googleMap.clear();
+                        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,10));
+                        googleMap.addMarker(markerOptions);
+
+                    }
+                });
+            }
+        });
+
         return root;
+
     }
+
+
+
 }
