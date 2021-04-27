b5666cfd84b637a3d3866e23567b82c3b296683a
dmitrybot <mandal437@yandex.ru>
2021-04-27 16:46:59 +0300

    Welcom_screen demo

--- app/src/main/java/shinepilates/app/pilatesapp/MainActivity.java
+++ app/src/main/java/shinepilates/app/pilatesapp/MainActivity.java
@@ -3,9 +3,11 @@ package shinepilates.app.pilatesapp;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import shinepilates.app.pilatesapp.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -18,12 +20,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity{
     private DrawerLayout drawer;
@@ -31,6 +35,7 @@ public class MainActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -39,12 +44,18 @@ public class MainActivity extends AppCompatActivity{
         NavigationView navigationView = findViewById(R.id.nav_view);
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_app_info, R.id.nav_news, R.id.nav_notifications, R.id.nav_maps, R.id.nav_treners, R.id.nav_contacts
-                , R.id.nav_homepage, R.id.nav_timetable, R.id.nav_report)
+                , R.id.nav_homepage, R.id.nav_timetable, R.id.nav_report, R.id.nav_welcom)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+        new Handler().postDelayed(new Runnable() {
+              @Override
+              public void run() {
+                  navController.navigate(R.id.nav_news);
+              }
+          }, 8000);
     }
 
     /*@Override
